e8013c0d6e016db164d1a7bc6c1355e05065bb38
jdelta1 <49237786+jdelta1@users.noreply.github.com>
2025-01-21 14:02:43 -0500

    Update qr-decoder.py

--- qr-decoder.py
+++ qr-decoder.py
@@ -17,14 +17,11 @@ def process_image(image_path):
 
 def main(folder_path):
     if os.path.isdir(folder_path):
-        # Open the output file
         with open("QrCodeValues.txt", "w") as output_file:
-            # Process each image in the folder
             for filename in os.listdir(folder_path):
                 if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
                     image_path = os.path.join(folder_path, filename)
                     decoded_data = process_image(image_path)
-                    # Write decoded data to the file
                     for line in decoded_data:
                         output_file.write(line + "\n")
         print("Processing complete. Results saved in QrCodeValues.txt")
